25f6cfd291afc36e63af100045e8441be939bedb
anaypaul <anay.paul2@gmail.com>
2019-01-06 21:20:22 -0700

    Codeforces Round #163 Div 2 A

---
+++ Contest Questions/DIV2/CFR_69_DIV2_A.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> isPrime(100+1,1);
+vector<int> primes;
+void seive(){
+    isPrime[0] = 0;
+    isPrime[1] = 0;
+    for(int i = 2; i<101; i++){
+        if(isPrime[i] == 1){
+            int j = 2;
+            while(i*j < 101){
+                isPrime[i*j] = 0;
+                j++;
+            }
+        }
+    }
+
+
+
+    for(int i=0;i<101;i++){
+        if(isPrime[i]==1){
+            primes.push_back(i);
+        }
+        
+    }
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    seive();
+    if(isPrime[n]==1 && isPrime[m]==1){
+        int i;
+        for(i = 0 ; i<primes.size();i++){
+            if(n== primes[i]){
+                break;
+            }
+        }
+        if(primes[i] == n && primes[i+1]==m){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
