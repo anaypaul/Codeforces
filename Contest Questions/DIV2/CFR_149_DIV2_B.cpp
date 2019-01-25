ee875a965ba2e0ed42ce53d2700198eefb02b28a
anaypaul <anay.paul2@gmail.com>
2019-01-25 13:07:23 -0700

    Adding solutions. ladder 1 complete.

--- Contest Questions/DIV2/CFR_149_DIV2_B.cpp
+++ Contest Questions/DIV2/CFR_149_DIV2_B.cpp
@@ -4,14 +4,30 @@
 #include<utility>
 using namespace std;
 int main(){
-    int n;
+    long long int n;
     cin>>n;
-    vector<pair<int,int> > segments;
-    for(int i = 0;i<n;i++){
-        int l,r;
+    long long int min_l = 9999999999;
+    long long int max_r =-9999999999;
+    cout<<"min: "<<min_l<<endl;
+    vector<pair<long long int,long long int> > segments;
+    for(long long int i = 0;i<n;i++){
+        long long int l,r;
         cin>>l>>r;
+        if(l < min_l ){
+            min_l = l;
+        }
+        if(r > max_r){
+            max_r = r;
+        }
         segments.push_back(make_pair(l,r));
     }
-    
+    // cout<<min_l<<" "<<max_r<<endl;
+    for(int i = 0;i<segments.size();i++){
+        if(segments[i].first <= min_l &&  segments[i].second >= max_r){
+            cout<<i+1<<endl;
+            return 0;
+        }
+    }
+    cout<<"-1"<<endl;
     return 0;
 }
---
+++ Contest Questions/DIV2/CFR_80_DIV2_A.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int required = 25 - n;
+    
+    return 0;
+}
